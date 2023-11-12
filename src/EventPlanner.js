07755ca0f7e6bfb7aa25db8607cbacb5667b3251
yu-retel <dbdudals100@naver.com>
2023-11-13 03:31:11 +0900

    feat: 12월 이벤트 확인 기능구현 및 필요한 파일 생성 EventPlanner Constants/event

---
+++ src/EventPlanner.js
@@ -0,0 +1,38 @@
+import EVENT from './Constants/event.js';
+
+class EventManager {
+  #visitday;
+  
+  #amount;
+
+  constructor(visitday, amount) {
+    this.#visitday = visitday;
+    this.#amount = amount;
+  }
+
+  #isChristmasEventApplied() {
+    if (this.#visitday >= 1 && this.#visitday <= 25) return true;
+    
+    return false;
+  }
+
+  #isWeekEventApplied() {
+    if (EVENT.weekendDays.inclduse(this.#visitday)) return true;
+
+    return false;
+  }
+
+  #isSpecialEventApplied() {
+    if (EVENT.specialDays.includes(this.#visitday)) return true;
+
+    return false;
+  }
+
+  #freeGiftEventApplied() {
+    if (this.#amount >= 120000) return true;
+
+    return false;
+  }
+}
+
+export default EventManager;
