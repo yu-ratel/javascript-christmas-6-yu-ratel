dec9ac440cbf374bffbcd942a74281080ffd150a
yu-retel <dbdudals100@naver.com>
2023-11-15 02:32:21 +0900

    fix: 예기치못한 오류로 인한 파일 대소문자 변경

---
+++ src/constants/event.js
@@ -0,0 +1,6 @@
+const EVENT = {
+  weekends: [1, 2, 8, 9, 15, 16, 22, 23, 29, 30],
+  specialDays: [3, 10, 17, 24, 25, 31],
+};
+
+export default EVENT;
---
+++ src/constants/menu.js
@@ -0,0 +1,25 @@
+const MENU = {
+  APPETIZER: {
+    mushroomSoup: { name: '양송이수프', price: 6000 },
+    tapas: { name: '타파스', price: 5500 },
+    caesarSalad: { name: '시저샐러드', price: 8000 },
+  },
+  MAIN: {
+    tBoneSteak: { name: '티본스테이크', price: 55000 },
+    bbqRibs: { name: '바비큐립', price: 54000 },
+    seafoodPasta: { name: '해산물파스타', price: 35000 },
+    christmasPasta: { name: '크리스마스파스타', price: 25000 },
+  },
+  DESSERT: {
+    chocoCake: { name: '초코케이크', price: 15000 },
+    iceCream: { name: '아이스크림', price: 5000 },
+  },
+  BEVERAGE: {
+    zeroCola: { name: '제로콜라', price: 3000 },
+    redWine: { name: '레드와인', price: 60000 },
+    champagne: { name: '샴페인', price: 25000 },
+  },
+};
+
+
+export default MENU;
