9655507c837555eb59f8b0f14c2db7c39f1077d4
yu-retel <dbdudals100@naver.com>
2023-11-14 14:05:58 +0900

    feat: 이벤트 적용 결과 출력 기능구현

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import InputView from './InputView.js';
 import OutputView from './OutputView.js';
 
+import OrderManager from './OrderManager.js';
+import EventPlanner from './EventPlanner.js';
+
+import visitDateInput from './validator/visitDateInput.js';
+
 class App {
   constructor() {
     this.InputView = InputView;
     this.OutputView = OutputView;
+    this.visitDate = null;
+    this.orderMenu = null;
   }
 
   async run() {
     this.OutputView.startMessage();
     await this.#requestEstimatedVisitDate();
     await this.#requestOrderMenu();
+    await this.#requestResult();
   }
 
   async #requestEstimatedVisitDate() {
-    const visitDate = await this.InputView.estimatedVisitDate();
-    return visitDate;
+    try {
+      const visitDate = await this.InputView.estimatedVisitDate();
+      visitDateInput.visitDateRange(visitDate);
+      this.visitDate = visitDate;
+      return null;
+    } catch(error) {
+      OutputView.print(error);
+      return this.#requestEstimatedVisitDate();
+    }
   }
   
   async #requestOrderMenu() {
     const orderMenu = await this.InputView.orderMenu();
-    return orderMenu;
+    this.orderMenu = new OrderManager(orderMenu);
+    
+  }
+
+  async #requestResult() {
+    OutputView.previewGuide(this.visitDate); 
+    OutputView.orderMenu(this.orderMenu.getOrderMenu());
+    const orderAmount = this.orderMenu.calculateTotalOrderAmount();
+    OutputView.orderAmount(orderAmount);
+
+    const event = new EventPlanner(this.visitDate, this.orderMenu.getOrderMenu(), orderAmount);
+    OutputView.freeGift(event.getIsFreeGift());
+
+    const discountAmount = event.calculateTotalDiscount();
+
+    OutputView.Benefits(event.eventState, discountAmount);
+    OutputView.discountAmount(discountAmount);
+    OutputView.discountedTotalAmount(this.orderMenu.calculateDiscountedTotalAmount(discountAmount));
+    OutputView.BadgeEvent(event.calculateBadgeEvent());
   }
 }
 
