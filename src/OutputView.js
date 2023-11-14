33477caecc26c8af6355b3f3ae0879d6cb4a9ba2
yu-retel <dbdudals100@naver.com>
2023-11-14 11:37:37 +0900

    feat: 혜택 미리보기 안내문 출력 기능구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -5,6 +5,59 @@ const OutputView = {
     this.print('안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.');
   },
 
+  previewGuide(visitday) {
+    this.print(`12월 ${visitday}에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`);
+  },
+
+  orderMenu(orderMenu) {
+    this.print('\n<주문 메뉴>');
+    
+    orderMenu.forEach((menu) => {
+      const [name, count] = menu;
+
+      this.print(`${name} ${count}개`);
+    });
+  },
+
+  orderAmount(amount) {
+    this.print(`\n<할인 전 총주문 금액>
+${amount}원`);
+  },
+
+  freeGift(isgift) {
+    this.print('\n<증정 메뉴>');
+
+    if (!isgift) return this.print('없음'); 
+
+    return this.print('샴페인 1개');
+  },
+
+  Benefits(eventState, discountAmount) {
+    this.print('\n<혜택 내역>');
+
+    if (discountAmount === 0) return this.print('없음');
+
+    return eventState.forEach((state) => {
+      const [message, discount] = state;
+      if (discount < 0) this.print(`${message}: ${discount}원`);
+    });
+  },
+
+  discountAmount(amount) {
+    this.print(`\n<총혜택 금액>
+${amount}원`);
+  },
+
+  discountedTotalAmount(amount) {
+    this.print(`\n<할인 후 예상 결제 금액>
+${amount}원`);
+  },
+
+  BadgeEvent(badge) {
+    this.print(`\n<12월 이벤트 배지>
+${badge}`);
+  },
+
   print(message) {
     Console.print(message);
   }
