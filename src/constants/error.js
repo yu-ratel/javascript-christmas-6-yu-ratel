fd8480b015c5f89588cd9c3ff0fc393df4d17d2e
yu-retel <dbdudals100@naver.com>
2023-11-15 02:52:58 +0900

    fix: 파일 인식불가로 인한 롤백

---
+++ src/constants/error.js
@@ -0,0 +1,10 @@
+const ERROR = {
+  MESSAGE: {
+    invalidDate: '[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.',
+    invalidOrder: '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.',
+    maxCount: '[ERROR] 메뉴는 최대 20개 까지만 주문 가능합니다. 다시 입력해 주세요.',
+    onlyBeverage: '[ERROR] 음료만 주문하실 순 없습니다. 다시 입력해 주세요.',
+  },
+};
+
+export default ERROR;
