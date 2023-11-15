f6d4ca5e9cb12eca9ddc08801e5d2392b333a66d
yu-retel <dbdudals100@naver.com>
2023-11-15 22:06:39 +0900

    refactor(messge): 중첩되는 toLocalString 메서드 추가

--- src/constants/messge.js
+++ src/constants/messge.js
@@ -1,7 +1,11 @@
+import { MENU } from './menu.js';
+
 const REQUSET = {
   visitDate: '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n',
   orderMenu: '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n',
 };
+
+const localString = (number) => number.toLocaleString();
 
 const MESSAGE = {
   start: '안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.',
@@ -9,15 +13,15 @@ const MESSAGE = {
   orderMenu: '\n<주문 메뉴>',
   orderMenuFormat: (name, count) => `${name} ${count}개`,
   orderAmount: (amount) => `\n<할인 전 총주문 금액>
-${amount.toLocaleString()}원`,
+${localString(amount)}원`,
   freeGift: '\n<증정 메뉴>',
-  freeGiftGiveaway: '샴페인 1개',
+  freeGiftGiveaway: `${MENU.BEVERAGE.champagne} 1개`,
   benefits: '\n<혜택 내역>',
-  benefitsFromate: (message, discount) => `${message}: ${discount.toLocaleString()}원`,
+  benefitsFromate: (message, discount) => `${message}: ${localString(discount)}원`,
   discountAmount: (amount) => `\n<총혜택 금액>
-${amount.toLocaleString()}원`,
+${localString(amount)}원`,
   discountedAmount: (amount) => `\n<할인 후 예상 결제 금액>
-${amount.toLocaleString()}원`,
+${localString(amount)}원`,
   badgeEvent: (badge) => `\n<12월 이벤트 배지>
 ${badge}`,
   none: '없음',
