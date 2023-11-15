83875b157be6745029a2c102eb597ea7f234783d
yu-retel <dbdudals100@naver.com>
2023-11-15 12:34:32 +0900

    feat(InputView): 문자열 상수화

--- src/InputView.js
+++ src/InputView.js
@@ -1,12 +1,13 @@
 import { Console } from '@woowacourse/mission-utils';
+import { REQUSET } from './constants/messge.js';
 
 const InputView = {
   estimatedVisitDate() {
-    return this.userInputRead('12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n');
+    return this.userInputRead(REQUSET.visitDate);
   },
 
   orderMenu() {
-    return this.userInputRead('주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n');
+    return this.userInputRead(REQUSET.orderMenu);
   },
 
   async userInputRead(userInput) {
