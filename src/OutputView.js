7e23a9e3682f47e596531d1f29142cad5f058733
yu-retel <dbdudals100@naver.com>
2023-11-15 12:59:41 +0900

    feat(OutputView): 문자열 상수화

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,61 +1,58 @@
 import { Console } from '@woowacourse/mission-utils';
+import { MESSAGE } from './constants/messge.js';
 
 const OutputView = {
   startMessage() {
-    this.print('안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.');
+    this.print(MESSAGE.start);
   },
 
   previewGuide(visitday) {
-    this.print(`12월 ${visitday}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`);
+    this.print(MESSAGE.preview(visitday));
   },
 
   orderMenu(orderMenu) {
-    this.print('\n<주문 메뉴>');
+    this.print(MESSAGE.orderMenu);
     
     orderMenu.forEach((menu) => {
       const [name, count] = menu;
 
-      this.print(`${name} ${count}개`);
+      this.print(MESSAGE.orderMenuFormat(name, count));
     });
   },
 
   orderAmount(amount) {
-    this.print(`\n<할인 전 총주문 금액>
-${amount.toLocaleString()}원`);
+    this.print(MESSAGE.orderAmount(amount));
   },
 
   freeGift(isgift) {
-    this.print('\n<증정 메뉴>');
+    this.print(MESSAGE.freeGift);
 
-    if (!isgift) return this.print('없음'); 
+    if (!isgift) return this.print(MESSAGE.none); 
 
-    return this.print('샴페인 1개');
+    return this.print(MESSAGE.freeGiftGiveaway);
   },
 
   Benefits(eventState, discountAmount) {
-    this.print('\n<혜택 내역>');
+    this.print(MESSAGE.benefits);
 
-    if (discountAmount === 0) return this.print('없음');
+    if (discountAmount === 0) return this.print(MESSAGE.none);
 
     return eventState.forEach((state) => {
       const [message, discount] = state;
-      if (discount < 0) this.print(`${message}: ${discount.toLocaleString()}원`);
+      if (discount < 0) this.print(MESSAGE.benefitsFromate(message, discount));
     });
   },
 
   discountAmount(amount) {
-    this.print(`\n<총혜택 금액>
-${amount.toLocaleString()}원`);
+    this.print(MESSAGE.discountAmount(amount));
   },
 
   discountedTotalAmount(amount) {
-    this.print(`\n<할인 후 예상 결제 금액>
-${amount.toLocaleString()}원`);
+    this.print(MESSAGE.discountedAmount(amount));
   },
 
   BadgeEvent(badge) {
-    this.print(`\n<12월 이벤트 배지>
-${badge}`);
+    this.print(MESSAGE.badgeEvent(badge));
   },
 
   print(message) {
